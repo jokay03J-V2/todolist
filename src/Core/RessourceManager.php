87ebd55fbe1f9dae658fa54b03c989b001e5572c
jokay03J-V2 <jmagro@edenschool.fr>
2023-11-24 16:17:32 +0100

    add js file load at body end

--- src/Core/RessourceManager.php
+++ src/Core/RessourceManager.php
@@ -2,9 +2,13 @@
 class RessourceManager
 {
     /**
-     * css files should be used on the header
+     * Css files should be used on the header
      */
     private array $cssFiles = [];
+    /**
+     * Js files should be used at end of the body
+     */
+    private array $jsFiles = [];
     /**
      * Base path ressource
      */
@@ -18,11 +22,19 @@ class RessourceManager
     }
 
     /**
-     * Add css file
+     * Add css file to be served on the head
      */
     function css(string $fileName)
     {
         array_push($this->cssFiles, $fileName);
+    }
+
+    /**
+     * Add js file to be served on body end
+     */
+    function js(string $fileName)
+    {
+        array_push($this->jsFiles, $fileName);
     }
 
     /**
@@ -31,6 +43,13 @@ class RessourceManager
     function getCSS()
     {
         return $this->cssFiles;
+    }
+    /**
+     * Get all js files
+     */
+    function getJS()
+    {
+        return $this->jsFiles;
     }
 }
 ?>
--- src/Views/layout.php
+++ src/Views/layout.php
@@ -21,9 +21,13 @@
 <body>
     <!-- Base layout for you views -->
     <?= $content ?>
+    <!-- Load all js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
+    <?php foreach ($ressources->getJS() as $key => $jsFile) { ?>
+        <script src="<?= $ressources->get($jsFile) ?>"></script>
+    <?php } ?>
 </body>
 
 </html>
