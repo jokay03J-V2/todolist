c511475ea4625307dadfe2bae53abc53a937e686
jokay03J-V2 <jmagro@edenschool.fr>
2023-11-24 15:46:32 +0100

    add autocomplete attributes

--- src/Views/auth/login.php
+++ src/Views/auth/login.php
@@ -14,11 +14,12 @@
         <form action="/login" method="POST">
             <div class="mb-3">
                 <label for="username" class="form-label">Surnom</label>
-                <input type="text" class="form-control" id="username" name="username" required>
+                <input type="text" class="form-control" id="username" name="username" autocomplete="username" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Mot de passe</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+                <input type="password" class="form-control" id="password" name="password"
+                    autocomplete="current-password" required>
             </div>
             <button type="submit" class="btn btn-primary">Se connecter</button>
             <hr>
