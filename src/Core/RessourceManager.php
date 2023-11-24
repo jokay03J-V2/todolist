87734e7c0a7f6f04b493a2190e3b2f3f8130ac04
jokay03J-V2 <jmagro@edenschool.fr>
2023-11-24 15:45:18 +0100

    add css file served should be served on the header

--- src/Core/RessourceManager.php
+++ src/Core/RessourceManager.php
@@ -1,13 +1,36 @@
 <?php
 class RessourceManager
 {
+    /**
+     * css files should be used on the header
+     */
+    private array $cssFiles = [];
+    /**
+     * Base path ressource
+     */
     public string $ressourcePath = "/src/Ressources/";
     /**
      * Get url for serve a file
      */
-    function get(string $filename)
+    function get(string $fileName)
     {
-        return CONFIG["ressources"]["hostname"] . $this->ressourcePath . $filename;
+        return CONFIG["ressources"]["hostname"] . $this->ressourcePath . $fileName;
+    }
+
+    /**
+     * Add css file
+     */
+    function css(string $fileName)
+    {
+        array_push($this->cssFiles, $fileName);
+    }
+
+    /**
+     * Get all css files
+     */
+    function getCSS()
+    {
+        return $this->cssFiles;
     }
 }
 ?>
--- src/Views/layout.php
+++ src/Views/layout.php
@@ -5,10 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todolist</title>
+    <!-- Bootstrap and material icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <!-- Load all css -->
+    <link rel="stylesheet" href="<?= $ressources->get("css/global.css") ?>">
+    <?php foreach ($ressources->getCSS() as $key => $cssFile) { ?>
+        <link rel="stylesheet" href="<?= $ressources->get($cssFile) ?>">
+    <?php } ?>
     <meta name="description" content="Todolist">
 </head>
 
