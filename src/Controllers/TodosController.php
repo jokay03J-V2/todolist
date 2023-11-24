b58ed78f26ed3f40317c6d3be77530d977e68e29
jokay03J-V2 <jmagro@edenschool.fr>
2023-11-24 15:22:52 +0100

    update date format, change sended status code

--- src/Controllers/TodosController.php
+++ src/Controllers/TodosController.php
@@ -6,8 +6,9 @@ class TodosController extends BaseController
      */
     function index(Request $request, Response $response)
     {
+        $userConnected = isset($_SESSION["user"]);
         // Check if user is connected, otherwise redirect to login page
-        if (!isset($_SESSION["user"])) {
+        if (!$userConnected) {
             $response->locateTo("/login");
             return $response;
         }
@@ -16,34 +17,37 @@ class TodosController extends BaseController
         // Get all todo
         $todos = $todoModel->getAll($_SESSION["user"]["id"]);
         // Render all todo
-        $this->view->render("home", ["todos" => $todos]);
+        $this->view->render("home", ["todos" => $todos, "userConnected" => $userConnected]);
     }
 
     /**
      * Update todo logic
+     * Note: This route is only used for AJAX request
      */
     function update(Request $request, Response $response)
     {
-        // Check if user is connected, otherwise redirect to login page
+        // Check if user is connected, otherwise send 401 code
         if (!isset($_SESSION["user"])) {
-            $response->locateTo("/login");
+            $response->json(["error" => ["message" => "You must be connected"]]);
+            $response->setStatus(401);
             return $response;
         }
 
         // Check if body include content and id
-        if (isset($request->body["content"]) && isset($request->body["id"])) {
+        if (isset($request->body["content"]) && strlen($request->body["content"]) <= 255 && isset($request->body["id"])) {
             $todoModel = new TodoModel();
+            $date = date("Y-m-d H:i:s");
             // Update todo
-            $hasBeenUpdated = $todoModel->updateById($request->body["id"], $_SESSION["user"]["id"], $request->body["content"], date("Y-m-d H:i:s"));
+            $hasBeenUpdated = $todoModel->updateById($request->body["id"], $_SESSION["user"]["id"], $request->body["content"], $date);
             // Check if todo has been updated
             if ($hasBeenUpdated) {
-                // Send "No content" http code
-                $response->setStatus(204);
+                // Send updated content
+                $response->json(["updatedAt" => date("d-m-Y H:i:s", strtotime($date)), "content" => htmlspecialchars($request->body["content"])]);
                 return $response;
             } else {
-                // Send error message with forbidden http code
-                $response->setStatus(403);
-                $response->json(["error" => ["message" => "Forbidden"]]);
+                // Send error message with 404 code
+                $response->setStatus(404);
+                $response->json(["error" => ["message" => "Todo not founded"]]);
                 return $response;
             }
         } else {
@@ -59,20 +63,23 @@ class TodosController extends BaseController
      */
     function create(Request $request, Response $response)
     {
+        $isConnected = isset($_SESSION["user"]);
         // Check if user is connected, otherwise redirect to login page
-        if (!isset($_SESSION["user"])) {
+        if (!$isConnected) {
             $response->locateTo("/login");
             return $response;
         }
         $todoModel = new TodoModel();
-        // Check if content has been provided
-        if (!empty($request->body["content"])) {
+        // Check if content has been provided and check if body is equal or lower than 255
+        if (!empty($request->body["content"]) && strlen($request->body["content"]) <= 255) {
+            // Add createdAt
+            $createdAt = date("Y-m-d H:i:s");
             // Create new todo
-            $todoModel->create($_SESSION["user"]["id"], htmlspecialchars($request->body["content"]));
+            $todoModel->create($_SESSION["user"]["id"], htmlspecialchars($request->body["content"]), $createdAt);
         }
         // Fetch all todos and render it
         $todos = $todoModel->getAll($_SESSION["user"]["id"]);
-        $this->view->render("home", ["todos" => $todos]);
+        $this->view->render("home", ["todos" => $todos, "userConnected" => $isConnected]);
     }
 
     /**
